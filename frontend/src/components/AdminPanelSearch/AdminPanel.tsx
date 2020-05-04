f50bdb03a5bcb09ce56b98e5d5d7f80b5fe90981
IvanPostu <ipostu20000127@gmail.com>
2020-05-04 14:33:18 -0400

    readme updated, frontend: onload page show bot info

--- frontend/src/components/AdminPanelSearch/AdminPanel.tsx
+++ frontend/src/components/AdminPanelSearch/AdminPanel.tsx
@@ -1,9 +1,17 @@
-import React, { FC, Fragment } from 'react'
+import React, { FC, Fragment, useEffect } from 'react'
 import { AdminPanelSearch } from '@/components/AdminPanelSearch/SearchForm/AdminPanelSearch'
 import { BotSubscribers } from '@/components/AdminPanelSearch/BotInfo/BotSubscribers'
 import { UserInfoAlertContainer } from '@/components/UserInfoAlert/UserInfoAlertContainer'
+import { useDispatch } from 'react-redux'
+import { Dispatch } from 'redux'
+import { fetchBot } from '@/store/Bot/actionCreators'
 
 export const AdminPanel: FC = () => {
+  const dispatch: Dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchBot(1))
+  }, [])
+
   return (
     <Fragment>
       <UserInfoAlertContainer />
